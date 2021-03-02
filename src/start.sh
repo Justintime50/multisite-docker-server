3d8e85949ff5708f03bd66d3c864dc9814155003
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-03-01 19:20:29 -0700

    Fixing start script

--- src/start.sh
+++ src/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 main() {
-    docker-compose -f traefik/insecure/docker-compose.yml up -d
-    docker-compose -f nginx-php-site/docker-compose.yml up -d
-    docker-compose -f mysql/docker-compose.yml up -d
+    docker-compose -f src/traefik/insecure/docker-compose.yml up -d
+    docker-compose -f src/nginx-php-site/docker-compose.yml up -d
+    docker-compose -f src/mysql/docker-compose.yml up -d
 }
 
 main
